6d781857233c1b9558626914666500349e461fb1
MinjunBark <94883504+MinjunBark@users.noreply.github.com>
2024-08-19 22:53:24 -0700

    lots of bugs and need fixing
    gui is not how I want it to be.
    
    needs to be padded side by side.
    window of gui should fit both images.

--- imageViewer.py
+++ imageViewer.py
@@ -1,42 +1,89 @@
-from PIL import Image, ImageTk
-import tkinter as tk 
+# from PIL import Image, ImageTk
+# import tkinter as tk 
 
+# class ImageViewer(tk.Frame):
+    
+#     def __init__(self, master, original_imgs, edited_imgs):
+#         super().__init__(master)
+#         self.original_imgs = original_imgs
+#         self.edited_imgs = edited_imgs
+#         self.current_image = 0
+        
+#         self.create_gui()
+        
+#     def create_gui(self):
+#         self.master.title("Image Viewer")
+        
+#         self.show_image()
+        
+#         self.prev_button = tk.Button(self, text='Prev', command=self.show_prev_image)
+#         self.prev_button.pack(side=tk.LEFT)
+        
+#         self.next_button = tk.Button(self, text="Next", command=self.show_next_image)
+#         self.next_button.pack(side=tk.RIGHT)
+        
+#         self.zoom_scale = tk.Scale(self, from_=1, to=10, orient=tk.HORIZONTAL, command=self.zoom_image)
+#         self.zoom_scale.pack(side=tk.BOTTOM)
+        
+#         self.pack()
+        
+#     def show_image(self):
+#         try:
+#             original_img = Image.open(self.original_imgs[self.current_image])
+#             edited_img = Image.open(self.edited_imgs[self.current_image])
+            
+#             original_tk_img = ImageTk.PhotoImage(original_img)
+#             edited_tk_img = ImageTk.PhotoImage(edited_img)
+            
+#             self.original_label = tk.Label(self, image=original_tk_img)
+#             self.original_label.image = original_tk_img
+#             self.original_label.pack()
+            
+#             self.edited_label = tk.Label(self, image=edited_tk_img)
+#             self.edited_label.image = edited_tk_img
+#             self.edited_label.pack()
+#         except FileNotFoundError:
+#             print("Error: File not found.")
+#         except Exception as e:
+#             print(f"An error occurred: {e}")
+        
+#     def show_prev_image(self):
+#         self.original_label.pack_forget()
+#         self.edited_label.pack_forget()
+        
+#         self.current_image = (self.current_image -1) % len(self.original_imgs)
+#         self.show_image()
+        
+#     def show_next_image(self):
+#         self.original_label.pack_forget()
+#         self.edited_label.pack_forget()
+        
+#         self.current_image = (self.current_image  + 1) % len(self.original_imgs)
+#         self.show_image()
+    
+#     def zoom_image(self, value):
+#         self.original_label.pack_forget()
+#         self.edited_label.pack_forget()
+        
+#         original_img = Image.open(self.original_imgs[self.current_image])
+#         edited_img = Image.open(self.edited_imgs[self.current_image])
+        
+#         value = int(value)
+        
+#         original_tk_img = ImageTk.PhotoImage(original_img.resize((int(original_img.width * value / 10), int(original_img.height * value / 10))))
+#         edited_tk_img = ImageTk.PhotoImage(edited_img.resize((int(edited_img.width * value / 10), int(edited_img.height * value / 10))))
+        
+#         self.original_label = tk.Label(self, image=original_tk_img)
+#         self.original_label.image = original_tk_img
+#         self.original_label.pack()
+        
+#         self.edited_label = tk.Label(self, image=edited_tk_img)
+#         self.edited_label.image = edited_tk_img
+#         self.edited_label.pack()
 
-class ImageViewer(tk.Frame):
-    
-    def __init__(self, master, original_imgs, edited_imgs):
-        super().__init__(master)
-        self.original_imgs = original_imgs
-        self.edited_imgs = edited_imgs
-        self.current_image = 0
-        self.create_widgets()
-    
-    def create_widgets(self):
-        self.canvas = tk.Canvas(self)
-        self.canvas.pack(fill=tk.BOTH, expand=True)
-        
-        self.prev_button = tk.Button(self, text='Prev', command=self.show_prev_image)
-        self.prev_button.pack(side=tk.LEFT)
-        
-        self.next_button = tk.Button(self, text="Next", command=self.show_next_image)
-        self.next_button.pack(side=tk.RIGHT)
-        
-        self.zoom_scale = tk.Scale(self, from=1, to=10, orient=tk.HORIZONTAL, command=self.zoom_image)
-        self.zoom_scale.pack(side=tk.BOTTOM)
-        
-        self.show_image()
-        
-    def show_image(self):
-        original_img = self.original_imgs[self.current_image]
-        edited_img = self.edited_imgs[self.current_image]
-        
-        original_tk_img = ImageTk.PhotoImage(original_img)
-        edited_tk_img = ImageTk.PhotoImage(edited_img)
-        
-        self.canvas.delete("all")
-        
-        self.canvas.create_image(0, 0, anchor=tk.NW, image=original_tk_img)
-        self.canvas.create_image(0, 0, anchor=tk.NW, image=edited_tk_img)
-        
-    def show_prev_image(self):
-        self.current_image
+# root = tk.Tk()
+# original_imgs = ["path/to/original/image1.jpg", "path/to/original/image2.jpg"]
+# edited_imgs = ["path/to/edited/image1.jpg", "path/to/edited/image2.jpg"]
+
+# viewer = ImageViewer(root, original_imgs, edited_imgs)
+# root.mainloop()
--- photoEditor.py
+++ photoEditor.py
@@ -1,5 +1,7 @@
-from PIL import Image, ImageEnhance, ImageFilter
+from PIL import Image, ImageEnhance, ImageFilter, ImageTk
 import os
+import tkinter as tk
+# from imageViewer import ImageViewer
 # import matplotlib.pyplot as plt
 
 
@@ -41,37 +43,96 @@ def adjust_contrast(img, file_name): #factor value: 0 - 10
     return edit
     
     
-# def show_image(original_imgs, edited_imgs):
-#     num_images = len(original_imgs)
-#     fig, ax = plt.subplots(2, num_images, figsize=(12, 6))
-    
-#     for i , (original_img, edited_img) in enumerate(zip(original_imgs, edited_imgs)):
-#         ax[0,i].imshow(original_img)
-#         ax[0,i].set_title(f'Original Image')
-#         ax[0,i].set_xticks([])
-#         ax[0,i].set_yticks([])
-        
-#         ax[1,i].imshow(edited_img)
-#         ax[1,i].set_title(f'Edited Image')
-#         ax[1,i].set_xticks([])
-#         ax[1,i].set_yticks([])
-#     plt.tight_layout()
-#     plt.show()
-
 def process_image():
+    root = tk.Tk()
     original_imgs = []
     edited_imgs = []
     for file in os.listdir(path):
         original_img = open_image(file)
         sharpened_img = adjust_sharpness(original_img, file)
-        # edited_imgs.append(sharpened_img)
         brightened_img =adjust_brightness(sharpened_img, file)
         contrasted_img = adjust_contrast(brightened_img, file)
         original_imgs.append(original_img)
         edited_imgs.append(contrasted_img)
     
+    # Set the maximum width and height for the images
+    max_width = 400
+    max_height = 300
+
+    # Resize the images
+    resized_original_imgs = []
+    resized_edited_imgs = []
+    for original_img, edited_img in zip(original_imgs, edited_imgs):
+        original_img.thumbnail((max_width, max_height))
+        edited_img.thumbnail((max_width, max_height))
+        resized_original_imgs.append(original_img)
+        resized_edited_imgs.append(edited_img)
+
+    # Get the dimensions of the image
+    image_width, image_height = resized_original_imgs[0].size
+
+    # Set the size of the GUI window to match the image size with some padding
+    root.geometry(f"{image_width+20}x{image_height+20}")
+
+    # Allow the user to resize the window
+    root.resizable(True, True)
     
-    show_image(original_imgs, edited_imgs)
+    original_frame = tk.Frame(root)
+    original_frame.pack(side=tk.LEFT)
+
+    edited_frame = tk.Frame(root)
+    edited_frame.pack(side=tk.RIGHT)
+
+    original_label = tk.Label(original_frame, text="Original Images")
+    original_label.pack()
+
+    edited_label = tk.Label(edited_frame, text="Edited Images")
+    edited_label.pack()
+
+    original_img_labels = []
+    edited_img_labels = []
+
+    for i, (original_img, edited_img) in enumerate(zip(resized_original_imgs, resized_edited_imgs)):
+        original_tk_img = ImageTk.PhotoImage(original_img)
+        original_img_label = tk.Label(original_frame, image=original_tk_img)
+        original_img_label.image = original_tk_img
+        original_img_label.pack()
+        original_img_labels.append(original_img_label)
+
+        edited_tk_img = ImageTk.PhotoImage(edited_img)
+        edited_img_label = tk.Label(edited_frame, image=edited_tk_img)
+        edited_img_label.image = edited_tk_img
+        edited_img_label.pack()
+        edited_img_labels.append(edited_img_label)
+
+    zoomed = False
+
+    def toggle_zoom():
+        nonlocal zoomed
+        for i, (original_img, edited_img) in enumerate(zip(resized_original_imgs, resized_edited_imgs)):
+            if not zoomed:
+                zoomed_original_img = original_img.resize((original_img.width * 2, original_img.height * 2))
+                zoomed_edited_img = edited_img.resize((edited_img.width * 2, edited_img.height * 2))
+                original_tk_img = ImageTk.PhotoImage(zoomed_original_img)
+                original_img_labels[i].config(image=original_tk_img)
+                original_img_labels[i].image = original_tk_img
+                edited_tk_img = ImageTk.PhotoImage(zoomed_edited_img)
+                edited_img_labels[i].config(image=edited_tk_img)
+                edited_img_labels[i].image = edited_tk_img
+            else:
+                original_tk_img = ImageTk.PhotoImage(original_img)
+                original_img_labels[i].config(image=original_tk_img)
+                original_img_labels[i].image = original_tk_img
+                edited_tk_img = ImageTk.PhotoImage(edited_img)
+                edited_img_labels[i].config(image=edited_tk_img)
+                edited_img_labels[i].image = edited_tk_img
+        zoomed = not zoomed
+
+    zoom_button = tk.Button(root, text="Zoom", command=toggle_zoom)
+    zoom_button.pack()
+
+    root.mainloop()
+
 
 
 if __name__ == '__main__':
